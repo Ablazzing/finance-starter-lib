29718eb64216558f01beecd4d6813756e188652d
YMolodyko <VTB4081847@region.vtb.ru>
2024-11-20 09:23:25 +0300

    second commit

--- finance-app/src/main/java/com/javaacademy/finance_app/Runner.java
+++ finance-app/src/main/java/com/javaacademy/finance_app/Runner.java
@@ -1,2 +1,15 @@
-package com.javaacademy.finance_app;public class Runner {
+package com.javaacademy.finance_app;
+
+import com.javaacademy.finance_starter.FinanceService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Runner {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Runner.class);
+        FinanceService financeService = context.getBean(FinanceService.class);
+        System.out.println();
+    }
 }
--- finance-app/src/test/java/com/javaacademy/finance_app/FinanceServiceTest.java
+++ finance-app/src/test/java/com/javaacademy/finance_app/FinanceServiceTest.java
@@ -1,2 +1,22 @@
-package com.javaacademy.finance_app;public class FinanceServiceTest {
+package com.javaacademy.finance_app;
+
+import com.javaacademy.finance_starter.FinanceService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@SpringBootTest
+public class FinanceServiceTest {
+    @Autowired
+    private FinanceService financeService;
+
+    @Test
+    public void successPay() {
+        assertDoesNotThrow(() -> financeService.takePayment(BigDecimal.TEN, "Любой текст"));
+    }
 }
